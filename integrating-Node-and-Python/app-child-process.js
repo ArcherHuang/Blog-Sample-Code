5f341c53a99b67240c0abc0751ec0fbdb855005c
ArcherHuang <mmosconii@gmail.com>
2019-11-28 10:07:44 +0800

    add Integrating Node and Python Code

---
+++ integrating-Node-and-Python/app-child-process.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+
+app.listen(3000, () => {
+  console.log('server running on port 3000')
+})
+
+// e.g., http://localhost:3000/call/python?name=Archer&from=Taipei
+// name = Archer
+// from = Taipei
+app.get('/call/python', pythonProcess)
+
+function pythonProcess(req, res) {
+
+  console.log(`name: ${req.query.name}, from: ${req.query.from}`)
+
+  let spawn = require("child_process").spawn
+
+  let process = spawn('python', [
+    "./process.py",
+    req.query.name,
+    req.query.from
+  ])
+
+  process.stdout.on('data', (data) => {
+    const parsedString = JSON.parse(data)
+    res.json(parsedString)
+  })
+
+} 
